ae3eeafb6c5c4e6493ea65651ea7fc9a6936d509
Vladislav <kolundaevva@gmail.com>
2022-07-28 16:28:58 +0300

    Initial Commit

---
+++ VK_handmade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_handmade
+//
+//  Created by Владислав Колундаев on 28.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
